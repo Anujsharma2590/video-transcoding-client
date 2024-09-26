d6f32effec3a986623ff7e2cd5f1f225271858fc
Anuj sharma <aanuj.sharma2590@gmail.com>
2024-09-26 21:55:06 +0530

    remove commneted code

--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -5,17 +5,14 @@ import { AuthContext } from "./AuthContext";
 const PrivateRoute = ({ children }) => {
   const { isAuthenticated, isLoading } = useContext(AuthContext);
 
-  // If still loading, show a loading spinner or placeholder
   if (isLoading) {
-    return <div>Loading...</div>; // Customize this to match your UI
+    return <div>Loading...</div>; 
   }
 
-  // If not authenticated, redirect to the login page
   if (!isAuthenticated) {
     return <Navigate to="/" />;
   }
 
-  // Otherwise, allow access to the protected component
   return children;
 };
 
