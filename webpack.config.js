1a82de5160aa7695893d113f14bf1c54cef9c2d4
Anuj sharma <aanuj.sharma2590@gmail.com>
2024-10-02 17:05:49 +0530

    Add images and files to my build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
-// webpack.config.js
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
-const webpack = require("webpack");
-const Dotenv = require("dotenv-webpack");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = (env) => {
   const isEnvDevelopment = env === "development";
@@ -46,9 +44,16 @@ module.exports = (env) => {
           test: /\.css$/,
           use: [
             "style-loader", // Injects styles into DOM
-            "css-loader", // Resolves @import and url()
+            "css-loader",   // Resolves @import and url()
             "postcss-loader", // Processes CSS with PostCSS
           ],
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|svg)$/,
+          type: "asset/resource", // Ensure images are treated as assets
+          generator: {
+            filename: "images/[hash][ext][query]", // Define where images are placed
+          },
         },
       ],
     },
@@ -56,11 +61,19 @@ module.exports = (env) => {
       new HtmlWebpackPlugin({
         title: "Transcoding App",
         template: path.join(__dirname, "public", "index.html"),
+        filename: "index.html", // Explicitly define the filename
       }),
       new Dotenv(),
+      // Copy files from public folder to build folder
       new CopyWebpackPlugin({
         patterns: [
-          { from: "public", to: "" }, 
+          {
+            from: path.resolve(__dirname, "public"),
+            to: path.resolve(__dirname, "build"), // Copy everything from public/ to build/
+            globOptions: {
+              ignore: ["**/index.html"], // Ignore index.html to avoid duplication
+            },
+          },
         ],
       }),
     ],
